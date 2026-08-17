71491e647bd3a3e170fd16e1c37f87132ebf33d1
AdarshSubrahmannyan <adarshh005@gmail.com>
2026-08-17 15:32:46 +0530

    Deploy portfolio to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,11 @@
 import type { NextConfig } from 'next';
-const nextConfig: NextConfig = { reactStrictMode: true };
+
+const nextConfig: NextConfig = {
+    reactStrictMode: true,
+    output: 'export',
+    images: {
+        unoptimized: true,
+    },
+};
+
 export default nextConfig;
